fb247ea102bcb4295ae26137b0d5008d3c16b4e1
HaciKandemir <hacikandemir2000@hotmail.com>
2020-11-12 19:25:33 +0300

    Uygulamadan çıkma eklendi.

--- Student_Management_V2/Menu.cs
+++ Student_Management_V2/Menu.cs
@@ -28,6 +28,9 @@ namespace Student_Management_V2
                         Show();
                     }
                     break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
             }
         }
 
