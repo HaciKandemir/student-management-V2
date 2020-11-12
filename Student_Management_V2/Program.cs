5fb603cc72807af1eaa390b4751a8a837f160ccc
HaciKandemir <hacikandemir2000@hotmail.com>
2020-11-12 08:59:13 +0300

    Proje dosyası ekle.

---
+++ Student_Management_V2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Student_Management_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
